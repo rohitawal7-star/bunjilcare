7cdaff1c3ab51649876ae9ddeaa66112277b1dc5
RohitAwal <rohit123awal@gmail.com>
2026-07-31 17:15:08 +0545

    Enable API routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   outputFileTracingRoot: process.cwd(),
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
